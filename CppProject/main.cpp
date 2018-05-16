e55802ff6a76f5baa4ef93c47eb4f65f7c9dbb53
yh0xa55a8 <yh0xa55a8@vip.qq.com>
2018-05-16 18:18:55 +0800

    完成文件路径获取保存

--- CppProject/main.cpp
+++ CppProject/main.cpp
@@ -6,5 +6,10 @@ int main(int argc, char *argv[])
 	QApplication a(argc, argv);
 	mainWindow w;
 	w.show();
+	std::u16string filePath;
+	QEventLoop fileLoop;
+	QObject::connect(&w, SIGNAL(fileOpened()), &fileLoop, SLOT(quit()));
+	fileLoop.exec();
+	filePath = w.getFilePath().toStdU16String();
 	return a.exec();
 }
