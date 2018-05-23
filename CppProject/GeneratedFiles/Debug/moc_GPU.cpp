86cd43b2fbe1a30e75e0a9b41a7d392806bc76f2
yh0xa55a8 <yh0xa55a8@vip.qq.com>
2018-05-23 20:30:26 +0800

    GPU部分有array越界错误 待排查

--- CppProject/GeneratedFiles/Debug/moc_GPU.cpp
+++ CppProject/GeneratedFiles/Debug/moc_GPU.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_GPU_t {
-    QByteArrayData data[1];
-    char stringdata0[4];
+    QByteArrayData data[3];
+    char stringdata0[16];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -31,10 +31,12 @@ struct qt_meta_stringdata_GPU_t {
     )
 static const qt_meta_stringdata_GPU_t qt_meta_stringdata_GPU = {
     {
-QT_MOC_LITERAL(0, 0, 3) // "GPU"
+QT_MOC_LITERAL(0, 0, 3), // "GPU"
+QT_MOC_LITERAL(1, 4, 10), // "freshImage"
+QT_MOC_LITERAL(2, 15, 0) // ""
 
     },
-    "GPU"
+    "GPU\0freshImage\0"
 };
 #undef QT_MOC_LITERAL
 
@@ -44,22 +46,41 @@ static const uint qt_meta_data_GPU[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       1,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   19,    2, 0x06 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::QImage,    2,
 
        0        // eod
 };
 
 void GPU::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
-    Q_UNUSED(_a);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        GPU *_t = static_cast<GPU *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->freshImage((*reinterpret_cast< QImage(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            typedef void (GPU::*_t)(QImage );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&GPU::freshImage)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
 }
 
 QT_INIT_METAOBJECT const QMetaObject GPU::staticMetaObject = {
@@ -84,7 +105,25 @@ void *GPU::qt_metacast(const char *_clname)
 int GPU::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
     return _id;
+}
+
+// SIGNAL 0
+void GPU::freshImage(QImage _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
